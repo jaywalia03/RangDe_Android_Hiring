9d5f08d8e5ec5c6a46f7064e1e772db6ff9f3c82
GauravSeta <gauravseta@gmail.com>
2017-05-13 14:53:03 +0530

    in mast2
    comment added

--- app/src/main/java/com/challenge/rangdeapp/MainActivity.java
+++ app/src/main/java/com/challenge/rangdeapp/MainActivity.java
@@ -15,7 +15,7 @@ import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-
+// by gaur
 import com.challenge.rangdeapp.services.RangDeService;
 
 public class MainActivity extends AppCompatActivity {
